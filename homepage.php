0d4b749e28220f4b842c5786e0609020a66a00c1
gaudris <gaudris@gmail.com>
2017-03-29 19:21:49 +0300

    Add files via upload

--- homepage.php
+++ homepage.php
@@ -10,32 +10,24 @@
 <link rel="stylesheet" href="css/style.css">
 </head>
 <body>
-	<!-- <div id="head-wrapper">
-		
-		<!-- <h3>Welcome    <?php echo $_SESSION['username']; ?></h3> -->
-		
-		<!-- <form action="index.php" method="post">
-			<div >
-				<button class="logout-btn" type="submit">Log Out</button>	
-			</div>
-		</form>
-	</div> --> 
 
 
-<div class="navig">
+
+<div id="navig">
 	
    <img src="css/pav/c-red.png">
+   <h2>Team</h2>
    <nav>
 
      <ul>
-		<li><a href="#">Index</a></li>
-		<li><a href="#">News</a>
-		<li><a href="#">About us</a>
+		<li><a href="#">Pagrindinis</a></li>
+		<li><a href="#">Naujienos</a>
+		<li><a href="#">Apie mus</a>
 			<ul>
-				<li><a href="#">Person 1</a></li>
-				<li><a href="#">Person 2</a></li>
-				<li><a href="#">Person 3</a></li>
-				<li><a href="#">Person 4</a></li>
+				<li><a href="#">Audrius</a></li>
+				<li><a href="#">Mantas</a></li>
+				<li><a href="#">Rolandas </a></li>
+				<li><a href="#">Patrikas</a></li>
 			</ul>
 		</li>
 		
@@ -51,19 +43,11 @@
 </div>
 	<div class="body">
 		<div>
-			<h1>Index</h1>
+			<h1>Pagrindinis</h1>
 			<img src="http://www.icode.es/wp-content/uploads/2012/10/01_database-480x328.jpg">
-			<p>Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod
-			tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam,
-			quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo
-			consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse
-			cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non
-			proident, sunt in culpa qui officia deserunt mollit anim id est laborum.Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod
-			tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam,
-			quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo
-			consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse
-			cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non
-			proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</p>
+			<p>Tai mūsų, Vilnius Coding Scool, mokinių grupes 13/02/17 , Komandos C, projektas, kuruo baigiame, intensivių programavimo kursų pradedantiesiems paskaitų bei praktinių dalių seriją.
+			Praleidę auditorijose ir namuose daugiau kaip šešias savaites sudalivavę trisdešimtyje pamokų, atlikę penkis projektus, išsprendę apie dušimtus užduočių ir prleidę šimtą dvidešimt akademinių valandų paskaitose ir dar daug laiko savarankiško darbo namuose ir padedami profesionalių mokyklos mokytojų, mes nemažai suvokėme ir įsisavinome pagrindinius programavimo principus, savyes ir subtilybes.
+			Per visą tą laiką, mes pramokome perskaityti, suprasti, ir užrašytitrumpą kodą, dirbti su duomenų bazėmis tiesiogiai ir iš programų vidaus, išmokome suprasti pagrindinius skirtumus tarp programavimo kalbų ir jų pritaikymo skirtinguose projektuose, jau geame parašyti serviso programą, kuri priima, apdoroja ir atida duomenis, dirbti prie projektų porose ir komandose, galime operuoti stiliais, suprasti matavimo vienetus, aprašus, paveldimumą, hierarchiją, taip pat jau gebame stilizuoti tekstus, formas, nuorodas, antraštes, paveiksliukus, skirtukus ir blokus, išmokome dirbti komandoje prie vieno didelio interneto projekto arba „Desktop“ mobiliosios programomis.</p>
 		</div>
          <hr>
 
@@ -71,14 +55,9 @@
 		<div class="pirmas">
 			<img src="http://scontent.cdninstagram.com/t51.2885-15/s480x480/e35/1515281_1699041753711792_1238440905_n.jpg?ig_cache_key=MTIxMjgwNDMyNTg3OTk5MzU5Mg%3D%3D.2">
 
-			<b>Vilnius Coding School</b>
+			<h3>Vilnius Coding School</h3>
 
-			<p>Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod
-			tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam,
-			quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo
-			consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse
-			cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non
-			proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</p>
+			<p>Programavimo mokykla „Vilnius Coding School“ inicijuojami intensyvūs tinklapio kūrimo mokymai jauniems, nedirbantiems ir nesimokantiems žmonėms, pasiryžusiems karjerą sieti su IT, įgauna pagreitį. Lietuvos darbo biržos (LDB) finansuojamus programavimo kursus nuo šiol galima lankyti ne tik Vilniuje, bet ir Kaune, o netrukus – ir Klaipėdoje. 160 valandų trukmės mokymų tikslas – ne tik išmokyti programavimo ir suteikti pakankamai žinių, kad žmogus galėtų pradėti programuoti ir įsidarbinti pradedančiuoju programuotoju, bet ir suteikti visokeriopą pagalbą ieškant darbo vietos. Būtent todėl, dar prieš kursų pradžią, mokiniai, kaip potencialūs kandidatai, yra siunčiami į pokalbį su įvairiomis IT įmonėmis, su kuriomis bendradarbiauja mokykla.</p>
  			<form action="http://www.vilniuscoding.lt" >
 			 	<div >
 					<button class="pirmas-btn" type="submit">Daugiau apie VCS</button>	
@@ -89,16 +68,11 @@
 	
 		<div class="antras">
 			<img src="http://funwebapp.weebly.com/uploads/3/2/1/2/32123941/732537118_orig.jpg?221g">
-			<b>Programavimo kalbos</b>
-			<p>Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod
-			tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam,
-			quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo
-			consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse
-			cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non
-			proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</p>
- 			<form action="index.php" >
+			<h3>Programavimo kalbos</h3>
+			<p>Pagrindinės VCS pradančiųjų progamuotojų išmokstamos programos: C# kalba – tai nors ir paprasta, tačiau yra pakankamai funkcionali. Tinkanti svetainėms, darbalaukiui ir mobilioms programoms kurti.  JAVA Tai viena populiariausių programavimo kalbų pasaulyje,naudojama kaip pagrindas, kuriant internetinių puslapių turinį bei mobiliąsias programas. HTML – tai paprasta ir lengvai išmokstama kalba tinklapiams kurti.CSS programavimo kalba išplečia HTML kalbos galimybes. Su šia kalba galima atvaizduoti teksto šriftą, tinklapio išdėstymą, spalvas, atstumą tarp elementų. CSS kalba leidžia tą patį stiliaus šabloną naudoti visuose tinklapio puslapiuose. Javascript kalba padės sukurti patogią naudoti, dinamišką svetainę. Su šia kalba galima kurti išskleidžiamuosius meniu, patvirtinimo lenteles, animaciją ir pan.MYSQL – tai standartizuota duomenų bazių valdymo kalba. Ji yra skirta redaguoti ir rasti informaciją, esančią tam tikrose duomenų bazių valdymo sistemose.</p>
+ 			<form action="http://www.codingdojo.com/blog/9-most-in-demand-programming-languages-of-2016" >
 				<div >
-				<button class="antras-btn" type="submit">Placiau apie kalbas</button>	
+				<button class="antras-btn" type="submit">Plačiau apie kalbas</button>	
 				</div>
 			</form>
 		</div>
@@ -108,22 +82,20 @@
 		<div class="trecias">
 			<img src="https://previews.123rf.com/images/ra2studio/ra2studio1405/ra2studio140500335/28029128-Hacker-programing-in-technology-enviroment-with-cyber-icons-and-symbols-Stock-Photo.jpg">
 			
-			<b>Paglbinės priemonės</b>
+			<h3>Pagalbinės priemonės</h3>
 			
-			<p>Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod
-			tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam,
-			quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo
-			consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse
-			cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non
-			proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</p>
- 			<form action="index.php">
+			<p>
+			Be paskaitoje pateiktos mokymosi medžiagos tokios kaip paskaitų skaidrės programų kodų pavyzdžiai ir pan., mes plačiai naudojomės ir išoriniais pagalbiniais internetopusapiais tokiais kaip <a href="https://stackoverflow.com">stackoverflow.com</a> tai pasaulinio lygio bendruomenė, besidalijanti programavimo ir problemų sprendimo patirtmi. Ttaip pat <a href="https://www.oracle.com/java/index.html">www.oracle.com/java</a>su didele JAVA kodų aprašymo duomenų baze. <a href="www.w3schools.com">www.w3schools.com</a>, tai pagalba dirbantims su tinklapių kūrimo programomis. <a href="https://wordpress.org/">WordPress.com</a> tai pagalbinė TVS tinklapių kūrimo programa. <a href="http://materializecss.com">http://materializecss.com</a> - tinklapių dizaino "framewokai". <a href="www.github.com">www.github.com</a> kodo duomenų keitimosi tarpusavyje platforma, dirbat komandoje su vienu projektu, Kitose situacijose ddziausias pagalbininkas <a href="www/google.com">www.google.com</a></p> <form action="https://colorlib.com/wp/free-css3-frameworks/">
 				<div >
-				<button class="trecias-btn" type="submit">Placiau apie pagalba</button>	
+				<button class="trecias-btn" type="submit">Plačiau apie pagalba</button>	
 				</div>
 			</form>
 		</div>
+		<hr>
+		<h1>...</h1>
+	
 
-
+</div>
 </div>
 
 
